11c2f0754db24720a4ad5964773752d2729ac3b7
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-06-28 23:01:50 +0600

    React router, tailwind installed successfully

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><h1 className='text-5xl'>Hello from My School</h1></div>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
